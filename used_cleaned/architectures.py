5069398063c6639390da0d1d302283e0c848e098
ChristianLahr <Chris@ChristinLahrMac.fritz.box>
2018-07-13 00:14:36 +0200

    used_cleaned folder

---
+++ used_cleaned/architectures.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+from tensorflow.contrib import layers
+import numpy as np
+from keras.layers import CuDNNGRU, Dropout, Bidirectional, BatchNormalization, SpatialDropout1D
+
+
+class CNN:
+
+    pass
+
+
+class CCNN:
+
+    pass
+
+
+class CRNN:
+
+    pass
+
+class BIRNN:
+
+    pass
+
+class CCAPS:
+
+    pass
+
+class CAPS:
+
+    pass
+
+class DENSE:
+
+    pass
+
+class CNNRNN:
+
+    pass
+
+class HYBRID:
+
+    pass
